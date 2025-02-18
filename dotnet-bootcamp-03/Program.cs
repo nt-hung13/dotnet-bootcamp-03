513ad64a020cd9181f0d03c7a0b581ea8a9438e5
Nguyễn Thành Hưng <ngthanhhung11@outlook.com>
2025-02-19 00:35:01 +0700

    Add project files.

---
+++ dotnet-bootcamp-03/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using dotnet_bootcamp_03;
+
+bt_buoi2.TotalInvoice();
---
+++ dotnet-bootcamp-03/bt-buoi2.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotnet_bootcamp_03
+{
+    internal class bt_buoi2
+    {
+        //Bài tập 1
+        static public void CoundDaysToWeek()
+        {
+            bool notNegative = true;
+            string numbOfDays = "";
+            while (notNegative)
+            {
+                int daysInput = 0;
+                Console.Write("input day(s): ");
+                numbOfDays = Console.ReadLine();
+                bool canConvert = int.TryParse(numbOfDays, out daysInput);
+                if(canConvert && daysInput > 0)
+                {
+                    if (daysInput < 7) Console.WriteLine($"{daysInput} day(s)");
+                    else Console.WriteLine($"{daysInput/7} week(s) {daysInput%7} day(s)"); 
+                    notNegative = false;
+                }
+                else
+                {
+                    Console.WriteLine("Not an number or the number is negative.");
+                }
+            }
+        }
+        
+        //Bài tập 2
+        static public void TotalInvoice()
+        {
+            Console.Write("Input price: ");
+            int price = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Input discount %: ");
+            decimal discountPercent = Convert.ToDecimal(Console.ReadLine())/100;
+            Console.WriteLine(discountPercent);
+
+            decimal total = price - (price * (discountPercent));
+            Console.WriteLine($"Total: {total}");
+        }
+    }
+}
