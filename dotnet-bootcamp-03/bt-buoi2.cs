41d2bfa72b69b0f049885e31386a03d07cb6a48f
Nguyễn Thành Hưng <ngthanhhung11@outlook.com>
2025-02-20 20:15:51 +0700

    bài tập buổi 2

--- dotnet-bootcamp-03/bt-buoi2.cs
+++ dotnet-bootcamp-03/bt-buoi2.cs
@@ -8,41 +8,117 @@ namespace dotnet_bootcamp_03
 {
     internal class bt_buoi2
     {
-        //Bài tập 1
+
+        /*
+        Bài tập 1: Tính số ngày trong tuần và số ngày lẻ
+        Input: 10
+        Ouput: 1 tuần 3 ngày
+         */
         static public void CoundDaysToWeek()
         {
-            bool notNegative = true;
-            string numbOfDays = "";
-            while (notNegative)
+            while (true)
             {
-                int daysInput = 0;
-                Console.Write("input day(s): ");
-                numbOfDays = Console.ReadLine();
-                bool canConvert = int.TryParse(numbOfDays, out daysInput);
-                if(canConvert && daysInput > 0)
+                Console.Write("Day(s): ");
+                bool canConvert = int.TryParse(Console.ReadLine(), out int daysInput);
+                if (canConvert && daysInput > 0)
                 {
                     if (daysInput < 7) Console.WriteLine($"{daysInput} day(s)");
-                    else Console.WriteLine($"{daysInput/7} week(s) {daysInput%7} day(s)"); 
-                    notNegative = false;
+                    else Console.WriteLine($"{daysInput / 7} week(s) {daysInput % 7} day(s)");
+                    break;
                 }
-                else
-                {
-                    Console.WriteLine("Not an number or the number is negative.");
-                }
+                Console.WriteLine("Please input an integer.");
             }
         }
-        
-        //Bài tập 2
+
+        /*
+        Bài tập 2: Tính tổng giá trị đơn hàng sau khi áp dụng giảm giá
+        Input: 100$ và 20% 
+        Output: 80$
+         */
         static public void TotalInvoice()
         {
-            Console.Write("Input price: ");
-            int price = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Input discount %: ");
-            decimal discountPercent = Convert.ToDecimal(Console.ReadLine())/100;
-            Console.WriteLine(discountPercent);
+            while (true)
+            {
+                Console.Write("Price: ");
+                bool priceInput = decimal.TryParse(Console.ReadLine(), out decimal priceAmount);
+                Console.Write("Sale %: ");
+                bool saleInput = int.TryParse(Console.ReadLine(), out int saleAmount);
+                Console.WriteLine($"{priceInput} and {saleInput} and {priceAmount} and {saleAmount}");
+                if ((priceInput && saleInput) && (priceAmount > 0 && saleAmount > 0))
+                {
+                    decimal totalDiscount = (priceAmount * saleAmount) / 100;
+                    Console.WriteLine($"Amount to pay: {priceAmount - totalDiscount}");
+                    break;
+                }
+                Console.WriteLine("Invalid input.");
+            }
+        }
 
-            decimal total = price - (price * (discountPercent));
-            Console.WriteLine($"Total: {total}");
+        /*
+        Bài tập 3: Chuyển đổi phút sang giờ và phút
+        Input: 130 phút
+        Output: 2 giờ 10 phút
+         */
+        static public void TimeConvertor()
+        {
+            while (true)
+            {
+                Console.Write("Minute(s): ");
+                bool minuteInput = int.TryParse(Console.ReadLine(), out int minutes);
+                if(minuteInput && minutes > 0)
+                {
+                    float _hours = (float)minutes / 60;
+                    int _minutes = minutes < 60 ? 0 : minutes % 60;
+                    Console.WriteLine($"{minutes} minute(s) = {_hours} hour(s) and {_minutes} minute(s)");
+                    break;
+                }
+                Console.WriteLine("Invalid input.");
+            }
+        }
+
+        /*
+         * Bài tập 4: Tính tổng số tiền sau khi cộng thêm thuế VAT
+         * Input: số tiền, số % VAT
+         * Output: tổng số tiền thanh toán
+         */
+        static public void SumAmountToPay()
+        {
+            while (true)
+            {
+                Console.Write("Amount (exclude VAT): ");
+                bool amountInput = int.TryParse(Console.ReadLine(),out int amount);
+                Console.Write("Input % VAT: ");
+                bool vatInput = int.TryParse(Console.ReadLine(), out int VAT);
+                if((amountInput && vatInput) && (amount > 0 && VAT > 0))
+                {
+                    int amountToPay = amount + ((amount * VAT) / 100);
+                    Console.WriteLine($"Amount to pay (include {VAT}% VAT): {amountToPay}");
+                    break;
+                }
+                Console.WriteLine("Invalid input.");
+            }
+        }
+
+        /*
+         *Bài tập 5: Chuyển đổi đơn vị tiền tệ
+         *Input: số tiền USD
+         *Output: số tiền VND tương ứng
+         */
+        static public void MoneyExchange()
+        {
+            int vndExchangeRate = 25000;
+            while (true)
+            {
+                Console.Write("USD amount: ");
+                bool usdInput = float.TryParse(Console.ReadLine(), out float usdAmount);
+                if (usdInput && usdAmount > 0)
+                {
+                    float vndAmount = usdAmount * vndExchangeRate;
+                    Console.WriteLine($"{usdAmount} USD = {vndAmount} VND");
+                    break;
+                }
+                Console.WriteLine("Invalid input.");
+            }
         }
     }
 }
